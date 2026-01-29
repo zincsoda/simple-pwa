4b158e81a9f5cfdc3bb68040ef21c9fe8e03a06e
Steve Walsh <steven.walsh39@gmail.com>
2026-01-29 09:04:44 +0800

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'pwa-cache-v2';
+const FILES_TO_CACHE = [
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
