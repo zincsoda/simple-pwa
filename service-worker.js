5406732b7f30620a54fc6b2e6767d430853bc1ca
Steve Walsh <stevewalsh@smartretail.co>
2026-02-01 17:54:50 +0800

    Changed add skip version

--- service-worker.js
+++ service-worker.js
@@ -22,3 +22,9 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
+self.addEventListener('message', event => {
+  if (event.data === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
